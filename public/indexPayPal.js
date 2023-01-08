94cb41c15ee5d20de056081ef46fea03a7b2969d
Santiago S <santiagoagredo@live.com>
2023-01-08 12:52:59 -0500

    Added error handling to paypal index

--- public/indexPayPal.js
+++ public/indexPayPal.js
@@ -2,13 +2,19 @@ paypal
         .Buttons({
           // Sets up the transaction when a payment button is clicked
           createOrder: async function (data, actions) {
-            return await fetch("/api/orders", {
-              method: "post",
-              // use the "body" param to optionally pass additional order information
-              // like product ids or amount
-            })
-              .then((response) => response.json())
-              .then((order) => order.id);
+            try {
+              return await fetch("/api/orders", {
+                method: "post",
+                // use the "body" param to optionally pass additional order information
+                // like product ids or amount
+              })
+                .then((response) => response.json())
+                .then((order) => order.id);
+                
+            } catch (error) {
+              console.log('Error coming from response')
+              console.log(error)
+            }
           },
           // Finalize the transaction after payer approval
           onApprove: async function (data, actions) {
